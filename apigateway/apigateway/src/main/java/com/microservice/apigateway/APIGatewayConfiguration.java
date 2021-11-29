65e333b00b3a1261a4fa2f64589de78d510cb362
Mukul Howale <howalemukul@gmail.com>
2021-11-29 14:18:05 +0530

    Microservice day 3

---
+++ apigateway/apigateway/src/main/java/com/microservice/apigateway/APIGatewayConfiguration.java
@@ -0,0 +1,23 @@
+package com.microservices.apigateway;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+
+@Configuration
+public class APIGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder)
+    {
+        Function<PredicateSpec, Buildable<Route>> routeFunction = P -> P.path("/get").uri("http://httpbin.org:80");
+
+        return builder.routes().route(routeFunction).build();
+    }
+}
